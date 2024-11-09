56afd79a7ca8f24225d183cd26613a1f698b15c9
Oyinkan Ifidon-Ola <olaa@umich.edu>
2024-11-09 14:29:53 -0500

    more wide screen updates

--- csv_to_meets_html.py
+++ csv_to_meets_html.py
@@ -208,7 +208,7 @@ def generate_image_tags(image_files, folder_path):
     for img in image_files:
         img_path = os.path.join(folder_path, img)
         # print(f"The image_path is {img_path}")
-        img_tags.append(f'<a href="../{img_path}" target="_blank" data-lightbox="gallery"><img src="../{img_path}" width="200" alt=""></a>')
+        img_tags.append(f'<a href="../{img_path}" target="_blank" data-lightbox="gallery"><img src="../{img_path}" width="200" alt="Image of {img_path} from the meet"></a>')
     return "\n".join(img_tags)
 
 # Putting it all together
